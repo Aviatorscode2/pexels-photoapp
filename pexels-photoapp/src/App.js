3a85c938662d672575eec71ed2a642b4da99fe24
Aviatorscode2 <victoryakubu5@gmail.com>
2022-01-14 04:20:17 +0100

    Dependencies and pexel search feature

---
+++ pexels-photoapp/src/App.js
@@ -0,0 +1,32 @@
+import Pexels from "./Pexels";
+import './App.css';
+
+function App() {
+
+    return ( <
+        Pexels > < /Pexels>
+    )
+
+    // const url = 'https://api.pexels.com/v1/search?query=searchText';
+
+    // const getPhotos = () => {
+    //       Axios.get(url, {
+    //           headers: { 'Authorization': '563492ad6f91700001000001e7a714553bb74edf96bf7757f128b36b'}
+    //       }).then(response =>{
+    //           console.log(response)
+    //       })
+    // }
+
+
+    // return ( <div className = "App" >
+    //     <header className = "App-header">
+    //         <h2>Pexels PhotoApp</h2>
+    //         <input className = "App-input" type = "text" placeholder = "Search..." />
+    //         <button onClick = { getPhotos }>Get</button>
+    //         <p>No photo</p>
+    //     </header> 
+    //     </div>
+    // );
+}
+
+export default App;
